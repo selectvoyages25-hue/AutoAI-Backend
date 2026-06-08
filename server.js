fcc0877105ac756eea8ccd3e6590bb2c50246037
Samir Bouras <selectvoyages25@gmail.com>
2026-06-08 23:50:14 +0200

    Use Render backend

--- server.js
+++ server.js
@@ -212,6 +212,8 @@ app.post("/analyze", async (req, res) => {
   });
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("🚀 SERVER OK http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 SERVER OK ${PORT}`);
 });
